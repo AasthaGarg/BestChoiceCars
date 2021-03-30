16dfa80d2907ce5fa42e9c087186865804e888c3
Aastha garg <bulbul.garg1997@gmail.com>
2021-03-30 21:34:19 +0530

    Create data.sql
    Using in-memory h2-database.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into Car(cid,brand,model,year,kms,price,fuel) values(101,'Hyundai','I10',2019,10000,300000,'Petrol');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(102,'Hyundai','I20',2018,20000,200000,'Diesel');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(103,'Hyundai','Verna',2019,15000,250000,'Petrol');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(104,'Tata','Bolt',2019,15000,250000,'Petrol');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(105,'Maruti Suzuki','Ciaz',2019,5000,600000,'Petrol');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(106,'Honda','jazz',2018,20000,200000,'Diesel');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(107,'Honda','City',2019,15000,400000,'');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(108,'Maruti Suzuki','Ertiga',2018,10000,550000,'Petrol');
+insert into Car(cid,brand,model,year,kms,price,fuel) values(109,'Tata','Nexon',2019,5000,600000,'Petrol');
+
